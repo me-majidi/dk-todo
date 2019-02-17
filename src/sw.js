5c6620925313e2273034bd8a17cb589879482685
Mehran Majidi <mehraanmajidi@yahoo.com>
2019-02-17 22:08:24 +0330

    Added noscript tag

--- src/sw.js
+++ src/sw.js
@@ -2,12 +2,12 @@ const cacheName = 'assets-v1';
 
 self.addEventListener('install', event => {
     console.log('installing ');
-    const preCache = async () => {
-        const cache = await caches.open('static-v1');
-        await cache.addAll(serviceWorkerOption.assets);
-    };
-    event.waitUntil(preCache());
-    console.log('installing called');
+    // const preCache = async () => {
+    //     const cache = await caches.open('static-v1');
+    //     await cache.addAll(serviceWorkerOption.assets);
+    // };
+    // event.waitUntil(preCache());
+    // console.log('installing called');
 });
 
 self.addEventListener('active', event => {
