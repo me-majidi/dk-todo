7075f77b6f7c5aa3b7413f398f915aaf6294e34e
Mehran Majidi <mehraanmajidi@yahoo.com>
2019-02-17 22:21:52 +0330

    Update ghpage

--- sw.js
+++ sw.js
@@ -4,11 +4,18 @@ var serviceWorkerOption = {
     ".fonts/dk-todo.svg",
     ".fonts/dk-todo.ttf",
     ".fonts/dk-todo.woff",
+    ".android-chrome-192x192.png",
+    ".android-chrome-512x512.png",
+    ".apple-touch-icon.png",
+    ".favicon-16x16.png",
+    ".favicon-32x32.png",
+    ".favicon.ico",
+    ".mstile-150x150.png",
     ".mobile.styles.css",
-    ".036a79dd84dd5deb030e.css",
-    ".036a79dd84dd5deb030e.js",
+    ".85d581b285a8ad1a716d.css",
+    ".85d581b285a8ad1a716d.js",
     ".index.html"
   ]
 };
         
-        !function(n){var r={};function o(e){if(r[e])return r[e].exports;var t=r[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,o),t.l=!0,t.exports}o.m=n,o.c=r,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(t,e){if(1&e&&(t=o(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)o.d(n,r,function(e){return t[e]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p=".",o(o.s=0)}([function(e,t){self.addEventListener("install",function(e){console.log("installing ")}),self.addEventListener("active",function(e){console.log("activated...")}),self.addEventListener("fetch",function(e){console.log(e.request.url)})}]);
+        !function(n){var r={};function o(e){if(r[e])return r[e].exports;var t=r[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,o),t.l=!0,t.exports}o.m=n,o.c=r,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(t,e){if(1&e&&(t=o(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)o.d(n,r,function(e){return t[e]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p=".",o(o.s=0)}([function(e,t){self.addEventListener("install",function(e){console.log("installing ")}),self.addEventListener("active",function(e){console.log("activated...")}),self.addEventListener("fetch",function(e){})}]);
